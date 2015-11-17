db6fe97867f72e6f4d1370a520a2241c25120091
Aaron Wright <wright_left@yahoo.com>
2015-11-16 20:22:28 -0800

    Trying again.

--- src/main/java/com/causticlabs/causticlabsmod/Steel.java
+++ src/main/java/com/causticlabs/causticlabsmod/Steel.java
@@ -3,17 +3,20 @@ package com.causticlabs.causticlabsmod;
 import org.apache.logging.log4j.Logger;
 import cofh.api.modhelpers.ThermalExpansionHelper;
 import cofh.lib.util.helpers.ItemHelper;
+import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.registry.GameData;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.oredict.OreDictionary;
 
 // Steel is only an alloy. It cannot be mined.
 public class Steel {
-
+   
    public static void apply(Logger logger) {
 
       // Remove all steel induction smelter recipes. We'll add our own.
       
-      // Don't allow iron dust and two coal dusts.
+      // Don't allow iron dust and two coal dusts. Trying both ways.
       ThermalExpansionHelper.removeSmelterRecipe(
          ItemHelper.stack(GameData.getItemRegistry().getObject("ThermalFoundation:material"), 1), 
          ItemHelper.stack(GameData.getItemRegistry().getObject("ThermalFoundation:material"), 2, 2));
@@ -21,6 +24,7 @@ public class Steel {
          ItemHelper.stack(GameData.getItemRegistry().getObject("ThermalFoundation:material"), 2, 2),
          ItemHelper.stack(GameData.getItemRegistry().getObject("ThermalFoundation:material"), 1));
       
+      /*
       // Don't allow iron ingot and two coal dusts.
       ThermalExpansionHelper.removeSmelterRecipe(
          ItemHelper.stack(GameData.getItemRegistry().getObject("minecraft:iron_ingot"), 1), 
@@ -35,11 +39,12 @@ public class Steel {
       ThermalExpansionHelper.removeSmelterRecipe(
          ItemHelper.stack(GameData.getItemRegistry().getObject("minecraft:iron_ingot"), 1), 
          ItemHelper.stack(GameData.getItemRegistry().getObject("ThermalFoundation:material"), 4, 3));
-      
+      */
       // Add induction smelter recipes to make steel. The smelter doesn't accept
       // ore dictionary ingredients. It'll take care of the ore dictionary stuff
       // itself.
       
+      /*
       // Make steel from invar dust and two coal dusts.
       ThermalExpansionHelper.addSmelterRecipe(
          8000, 
@@ -95,6 +100,6 @@ public class Steel {
          ItemHelper.stack(GameData.getItemRegistry().getObject("minecraft:coal"), 4, 1), 
          ItemHelper.stack(GameData.getItemRegistry().getObject("ThermalFoundation:material"), 1, 72), 
          ItemHelper.stack(GameData.getItemRegistry().getObject("TConstruct:materials"), 1, 16));
-      
+      */
    }
 }
